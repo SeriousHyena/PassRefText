f260d49da4dcddd9bb9a0d73bf87c9d403709587
SeriousHyena <emackiii@gmail.com>
2021-10-15 08:53:09 -0700

    reformatted output

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ void addText(string& output, string input);
 
 
 //declare variables here
-string inputString;									//local scope variable declaration
+string inputString = "Peanut!";						//local scope variable declaration
+string foo;
 
 int main()
 {
 	void addText(string& output, string input);		//invoke the function
 
-	cout << "Bailey" << endl;						//print ou the local variable
-	addText(inputString, " ");
-	cout << inputString;							//print out the passed variable
+	cout << "The local scope (reference variable) is: "<< inputString << endl;
+
+	addText(foo, inputString);
+	cout << "The first word: " << foo << "is a passed variable. " << "The second word: " << inputString
+		<< " is a reference (local) variable." << endl;;							//print out the passed variable
 
 	return 0;
 }
