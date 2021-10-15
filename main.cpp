b1b1e84c150361211dd49576a9876dbf190b3f9a
SeriousHyena <emackiii@gmail.com>
2021-10-15 08:40:39 -0700

    final

--- main.cpp
+++ main.cpp
@@ -5,16 +5,24 @@ using namespace std;
 
 
 //declare functions here
-void addText(string input, string& output);
+void addText(string& output, string input);
+
+
+//declare variables here
+string inputString;									//local scope variable declaration
 
 int main()
 {
-	void addText(string input, string & output);
+	void addText(string& output, string input);		//invoke the function
+
+	cout << "Bailey" << endl;						//print ou the local variable
+	addText(inputString, " ");
+	cout << inputString;							//print out the passed variable
 
 	return 0;
 }
 
-void addText(string input, string& output)
+void addText(string& output, string input)
 {
-
+	output = "Hello! ";								//passed scope variable declaration
 }
