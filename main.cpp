b685d21030500aed45bbe6ec02ba3a78fde87bbe
SeriousHyena <emackiii@gmail.com>
2021-10-15 08:07:50 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//declare functions here
+void addText(string input, string& output);
+
+int main()
+{
+	void addText(string input, string & output);
+
+	return 0;
+}
+
+void addText(string input, string& output)
+{
+
+}
